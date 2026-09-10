ba1208bc7a219e928c62179d72e3136ff9c77470
ALyasi <r.m.alyasi@gmail.com>
2026-09-10 22:39:05 +0400

    Load bilingual verdict editor on event create

--- resources/views/admin/events/create.blade.php
+++ resources/views/admin/events/create.blade.php
@@ -33,5 +33,12 @@
 @endsection
 
 @push('scripts')
+    <script>
+        window.__eventUpgradeVerdict = {
+            ar: @json(old('upgrade_verdict_text_ar', '')),
+            en: @json(old('upgrade_verdict_text_en', ''))
+        };
+    </script>
+    <script src="{{ versioned_asset('assets/admin/js/event-verdict-localization.js') }}" defer></script>
     <script src="{{ versioned_asset('assets/admin/js/event-pricing-auto.js') }}" defer></script>
 @endpush
