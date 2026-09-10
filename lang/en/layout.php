7e469cc8d3766a496ac8d320ff3f410948877d87
ALyasi <r.m.alyasi@gmail.com>
2026-09-10 11:31:57 +0400

    Rename community navigation to events

--- lang/en/layout.php
+++ lang/en/layout.php
@@ -8,6 +8,7 @@ return [
         'works' => 'Our Work',
         'news' => 'News',
         'articles' => 'My Articles',
+        'events' => 'Events',
         'community' => 'Community Events',
     ],
 
@@ -18,9 +19,10 @@ return [
     'menu_close' => 'Close menu',
 
     'footer' => [
-        'about' => 'A platform bringing together digital services, tech news, and community in one integrated experience.',
+        'about' => 'A platform bringing together digital services, tech news, conferences, and events in one integrated experience.',
         'quick_links' => 'Quick Links',
         'community_title' => 'Community',
+        'explore_title' => 'Explore',
         'faq' => 'FAQ',
         'contact_us' => 'Contact Us',
         'careers' => 'Careers',
